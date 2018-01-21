ba369565f31c12dde83ec0b2d4f8bb070a22afca
Margar Khnusyan <xnusyan1992@mail.com>
2018-01-22 01:04:48 +0400

    change in views

--- resources/views/test.blade.php
+++
@@ -1,7 +0,0 @@
-<div style="background-color: green;height: 100%;width: auto;padding: 40px 40px;text-align: center">
-    <p style="color: white;font-size: 16px">Hello this project is called - users chat</p>
-    <p style="color: white;font-size: 16px">I start my work</p>
-    <h2 style="color: whitesmoke">Test breanch</h2>
-    <h3 style="color: gray;font-weight: 900">Test commit</h3>
-    <h3 style="color: whitesmoke; color: red" >Test commit</h3>
-</div>
